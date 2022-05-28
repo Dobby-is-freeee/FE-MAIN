c239beeb38b4de1c685f54868e5f6fdb8cb9dbff
chkim116 <dudnqnfqlc@naver.com>
2022-05-28 22:27:03 +0900

    feat: 공통 컴포넌트 작업을 위한 세팅
    - box-sizing 추가
    - sampleImage 변수화
    - 로그인 유지 로직 추가 (임시)

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,12 +1,24 @@
 import { Studio } from '@/features/studio';
+import { Layouts } from '@/features/studio/components';
+import { Outlet } from 'react-router-dom';
 
 export const protectedRoutes = [
   {
     path: '/',
-    element: <Studio />,
-  },
-  {
-    path: '*',
-    element: <div>empty</div>,
+    element: (
+      <Layouts>
+        <Outlet />
+      </Layouts>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <Studio />,
+      },
+      {
+        path: '*',
+        element: <div>empty</div>,
+      },
+    ],
   },
 ];
