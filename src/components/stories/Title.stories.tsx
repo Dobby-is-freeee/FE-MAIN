938f03a3b6f138d1517ceb7281d141e02ade21ca
chkim116 <dudnqnfqlc@naver.com>
2022-06-26 20:45:05 +0900

    feat: 타이틀 스토리북 외 스니펫 추가

---
+++ src/components/stories/Title.stories.tsx
@@ -0,0 +1,40 @@
+import { ArgTypes, ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentProps } from 'react';
+
+import { Title } from '../TypoGraphy/Title';
+
+type MyArgTypes = Partial<Record<keyof ComponentProps<typeof Title>, ArgTypes[string]>>;
+
+const argTypes: MyArgTypes = {};
+
+export default {
+  component: Title,
+  title: 'components/TypoGraphy/Title',
+  argTypes,
+} as ComponentMeta<typeof Title>;
+
+const Template: ComponentStory<typeof Title> = (args) => <Title {...args} />;
+
+export const Head1 = Template.bind({});
+Head1.args = {
+  level: 1,
+  children: '헤딩 1',
+};
+
+export const Head2 = Template.bind({});
+Head2.args = {
+  level: 2,
+  children: '헤딩 2',
+};
+
+export const Head3 = Template.bind({});
+Head3.args = {
+  level: 3,
+  children: '헤딩 3',
+};
+
+export const Head4 = Template.bind({});
+Head4.args = {
+  level: 4,
+  children: '헤딩 4',
+};
