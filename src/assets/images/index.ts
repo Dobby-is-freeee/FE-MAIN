6009f3e248b72df7966d6eed8864ca6b97bdfbc3
changhoe kim <dudnqnfqlc@naver.com>
2022-07-30 00:01:46 +0900

    refactor: 사용하는 아이콘 제외 svg import로 롤백

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,36 +1,39 @@
-import { ReactComponent as AddProfile } from '../images/ic_add_profile.svg';
-import { ReactComponent as Calendar } from '../images/ic_calendar.svg';
-import { ReactComponent as Close } from '../images/ic_close.svg';
-import { ReactComponent as Confirm } from '../images/ic_confirm.svg';
-import { ReactComponent as Dropdown } from '../images/ic_dropdown.svg';
-import { ReactComponent as Error1 } from '../images/ic_error-1.svg';
-import { ReactComponent as Error } from '../images/ic_error.svg';
-import { ReactComponent as EyeHidden } from '../images/ic_eye_hidden.svg';
-import { ReactComponent as EyeOpen } from '../images/ic_eye_n.svg';
-import { ReactComponent as Google } from '../images/ic_google.svg';
-import { ReactComponent as Icon01 } from '../images/ic_icon01.svg';
-import { ReactComponent as Icon02 } from '../images/ic_icon02.svg';
-import { ReactComponent as Icon03 } from '../images/ic_icon03.svg';
-import { ReactComponent as Icon04 } from '../images/ic_icon04.svg';
-import { ReactComponent as Icon05 } from '../images/ic_icon05.svg';
-import { ReactComponent as Icon06 } from '../images/ic_icon06.svg';
-import { ReactComponent as Icon07 } from '../images/ic_icon07.svg';
-import { ReactComponent as Icon08 } from '../images/ic_icon08.svg';
-import { ReactComponent as Link } from '../images/ic_link.svg';
-import { ReactComponent as Mail } from '../images/ic_mail.svg';
+import AddProfile from '../images/ic_add_profile.svg';
+import Calendar from '../images/ic_calendar.svg';
+import Close from '../images/ic_close.svg';
+import Confirm from '../images/ic_confirm.svg';
+import Dropdown from '../images/ic_dropdown.svg';
+import Error1 from '../images/ic_error-1.svg';
+import Error from '../images/ic_error.svg';
+import EyeHidden from '../images/ic_eye_hidden.svg';
+import EyeOpen from '../images/ic_eye_n.svg';
+import Google from '../images/ic_google.svg';
+import Icon02 from '../images/ic_icon02.svg';
+import Icon03 from '../images/ic_icon03.svg';
+import Icon04 from '../images/ic_icon04.svg';
+import Icon05 from '../images/ic_icon05.svg';
+import Icon06 from '../images/ic_icon06.svg';
+import Icon07 from '../images/ic_icon07.svg';
+import Icon08 from '../images/ic_icon08.svg';
+import Link from '../images/ic_link.svg';
+import Mail from '../images/ic_mail.svg';
+import MyProfilePurple from '../images/ic_my_profile_p.svg';
+import Notice from '../images/ic_notice.svg';
+import ProfileDefaultMedium from '../images/ic_profile_default_m.svg';
+import ProfileDefaultSmall from '../images/ic_profile_default_s.svg';
+import ProjectPurple from '../images/ic_project_p.svg';
+import Star from '../images/ic_star.svg';
+import LogoLarge from '../images/logo_l.svg';
+import LogoSmall from '../images/logo_s.svg';
+
+// react component로 사용하기 위해 뺐습니다.
 import { ReactComponent as Member } from '../images/ic_member.svg';
 import { ReactComponent as MyProfileBlack } from '../images/ic_my_profile_b.svg';
-import { ReactComponent as MyProfilePurple } from '../images/ic_my_profile_p.svg';
-import { ReactComponent as Notice } from '../images/ic_notice.svg';
-import { ReactComponent as ProfileDefaultMedium } from '../images/ic_profile_default_m.svg';
-import { ReactComponent as ProfileDefaultSmall } from '../images/ic_profile_default_s.svg';
-import { ReactComponent as ProjectBlack } from '../images/ic_project_b.svg';
-import { ReactComponent as ProjectPurple } from '../images/ic_project_p.svg';
-import { ReactComponent as Star } from '../images/ic_star.svg';
-import { ReactComponent as LogoLarge } from '../images/logo_l.svg';
-import { ReactComponent as LogoSmall } from '../images/logo_s.svg';
 import { ReactComponent as SettingBlack } from '../images/ic_setting_b.svg';
 import { ReactComponent as SettingPurple } from '../images/ic_setting_p.svg';
+import { ReactComponent as ProjectBlack } from '../images/ic_project_b.svg';
+
+import { ReactComponent as Icon01 } from '../images/ic_icon01.svg';
 
 export {
   AddProfile,
