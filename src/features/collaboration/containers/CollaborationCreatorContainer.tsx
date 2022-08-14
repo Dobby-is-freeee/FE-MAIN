f062e5993b3995c8251295f9d7d2806e01073d5e
chkim116 <dudnqnfqlc@naver.com>
2022-08-14 21:07:23 +0900

    feat: rc-time-picker 기반 일정 선택 구현

--- src/features/collaboration/containers/CollaborationCreatorContainer.tsx
+++ src/features/collaboration/containers/CollaborationCreatorContainer.tsx
@@ -1,4 +1,24 @@
+import { Day } from '@hassanmojab/react-modern-calendar-datepicker';
+import { useState } from 'react';
 import { ProjectRoomCreator } from '../components/collaboration/ProjectRoomCreator';
+
+// FIXME: 서버에게 어떤 값을 줄지 몰라 임의로 설정한 타입임.
+export interface RoomDateModel extends Day {
+  hour: number;
+  minute: number;
+}
+
+function createRooDateModel(): RoomDateModel {
+  const today = new Date();
+
+  return {
+    year: today.getFullYear(),
+    month: today.getMonth() + 1,
+    day: today.getDate(),
+    hour: 0,
+    minute: 0,
+  };
+}
 
 interface ProjectRoomCreatorProps {
   isCreatorVisible: boolean;
@@ -9,5 +29,19 @@ export const CollaborationCreatorContainer = ({
   isCreatorVisible,
   onCreatorVisibleToggle,
 }: ProjectRoomCreatorProps) => {
-  return <ProjectRoomCreator onCreatorVisibleToggle={onCreatorVisibleToggle} isCreatorVisible={isCreatorVisible} />;
+  const [roomDate, setRoomDate] = useState<RoomDateModel>(createRooDateModel());
+
+  const handleDateChange = (state: Partial<RoomDateModel>) => {
+    // TODO: 서버 로직
+    setRoomDate((prev) => ({ ...prev, ...state }));
+  };
+
+  return (
+    <ProjectRoomCreator
+      roomDate={roomDate}
+      onDateChange={handleDateChange}
+      onCreatorVisibleToggle={onCreatorVisibleToggle}
+      isCreatorVisible={isCreatorVisible}
+    />
+  );
 };
